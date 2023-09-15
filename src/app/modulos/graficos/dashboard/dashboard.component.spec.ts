3943dccdc24e5b715d2eb20d8607ec4c32e04153
thi4go-git <thi4go19@gmail.com>
2023-09-15 03:42:32 -0300

    version

--- src/app/modulos/graficos/dashboard/dashboard.component.spec.ts
+++ src/app/modulos/graficos/dashboard/dashboard.component.spec.ts
@@ -17,7 +17,7 @@ describe('DashboardComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('deve criar', () => {
     expect(component).toBeTruthy();
   });
 });
