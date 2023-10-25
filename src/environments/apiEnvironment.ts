0c40d2f2525c4bd430e6ea27526100e507ceab29
thi4go-git <thi4go19@gmail.com>
2023-10-25 09:48:47 -0300

    Ajustes

--- src/environments/apiEnvironment.ts
+++ src/environments/apiEnvironment.ts
@@ -1,7 +1,7 @@
 export const apiEnvironment = {
     production: false,
     //apiUrl: 'http://localhost:8080',
-    apiUrl: 'http://cloudtecnologia.dynns.com:8085',
+    apiUrl: 'http://cloudtecnologia.dynns.com:8085', 
     tokenUrl: 'http://cloudtecnologia.dynns.com:8180/realms/CLOUD_TECNOLOGIA/protocol/openid-connect/token',
     versao: 'v_19.10.2023',
     cli_id: 'fluxo-caixa-client',
