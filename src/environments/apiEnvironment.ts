5af5eb12a537bc8979b7eeea1222ed3008f3c3cf
thi4go-git <thi4go19@gmail.com>
2023-10-28 11:53:42 -0300

    Teste

--- src/environments/apiEnvironment.ts
+++ src/environments/apiEnvironment.ts
@@ -1,6 +1,6 @@
 export const apiEnvironment = {
     production: false,
-    //apiUrl: 'http://localhost:8080',
+    // apiUrl: 'http://localhost:8080',
     apiUrl: 'http://cloudtecnologia.dynns.com:8085', 
     tokenUrl: 'http://cloudtecnologia.dynns.com:8180/realms/CLOUD_TECNOLOGIA/protocol/openid-connect/token',
     versao: 'v_19.10.2023',
