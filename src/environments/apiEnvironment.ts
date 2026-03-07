76c6ab88b26f1626b80ffb5b1c05075aa63625a0
Thiago Melo <thiagomelo@saude-go.net>
2026-03-07 18:57:56 -0300

    Apontando para back + keycloak HTTPS

--- src/environments/apiEnvironment.ts
+++ src/environments/apiEnvironment.ts
@@ -1,9 +1,9 @@
 export const apiEnvironment = {
     production: false,
-    //apiUrl: 'http://localhost:8080',
-    apiUrl: 'http://cloudtecnologia.dynns.com:8001',
-    tokenUrl: 'http://cloudtecnologia.dynns.com:8180/realms/CLOUD_TECNOLOGIA/protocol/openid-connect/token',
-    versao: 'v_19.11.2025',
+    apiUrl: 'https://fluxocaixa-api.ddns.net/', //'http://cloudtecnologia.dynns.com:8001'    -   'http://localhost:8080'
+    //tokenUrl: 'http://cloudtecnologia.dynns.com:8180/realms/CLOUD_TECNOLOGIA/protocol/openid-connect/token',
+    tokenUrl: 'https://cloudtecnologia.dynns.com/auth/realms/CLOUD_TECNOLOGIA/protocol/openid-connect/token',
+    versao: 'v_07.03.2026',
     cli_id: 'fluxo-caixa-client',
     cli_secret: 'q80HxrDVjoX93hdA81g3JI4L61FUO2T9'
 };
