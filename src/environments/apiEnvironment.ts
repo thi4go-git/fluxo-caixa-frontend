26a0d1d3c386276c0db7b40ba65a10b2ace8252f
Thiago Melo <thiagomelo@saude-go.net>
2026-03-08 05:04:35 -0300

    Correção variável

--- src/environments/apiEnvironment.ts
+++ src/environments/apiEnvironment.ts
@@ -1,7 +1,6 @@
 export const apiEnvironment = {
     production: false,
-    apiUrl: 'https://fluxocaixa-api.ddns.net/', //'http://cloudtecnologia.dynns.com:8001'    -   'http://localhost:8080'
-    //tokenUrl: 'http://cloudtecnologia.dynns.com:8180/realms/CLOUD_TECNOLOGIA/protocol/openid-connect/token',
+    apiUrl: 'https://fluxocaixa-api.ddns.net',
     tokenUrl: 'https://cloudtecnologia.dynns.com/auth/realms/CLOUD_TECNOLOGIA/protocol/openid-connect/token',
     versao: 'v_07.03.2026',
     cli_id: 'fluxo-caixa-client',
