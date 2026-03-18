e265f06bbf6bde965aa53564f95487081d3769c7
Thiago Melo <thiagomelo@saude-go.net>
2026-03-18 00:32:19 -0300

    Ajuste environment

--- src/environments/apiEnvironment.ts
+++ src/environments/apiEnvironment.ts
@@ -1,7 +1,7 @@
 export const apiEnvironment = {
     production: false,
-    apiUrl: 'https://fluxocaixa-api.ddns.net',
-    tokenUrl: 'https://cloudtecnologia.dynns.com/auth/realms/CLOUD_TECNOLOGIA/protocol/openid-connect/token',
+    apiUrl: 'http://cloudtecnologia.dynns.com:8001',
+    tokenUrl: 'http://cloudtecnologia.dynns.com:8180/realms/CLOUD_TECNOLOGIA/protocol/openid-connect/token',
     versao: 'v_07.03.2026',
     cli_id: 'fluxo-caixa-client',
     cli_secret: 'q80HxrDVjoX93hdA81g3JI4L61FUO2T9'
