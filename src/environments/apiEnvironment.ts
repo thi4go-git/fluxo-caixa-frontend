04a12081fafd3dfa534a5442cc8d60b9365c3613
Thiago Melo <thiagomelo@saude-go.net>
2026-03-18 02:14:26 -0300

    Version

--- src/environments/apiEnvironment.ts
+++ src/environments/apiEnvironment.ts
@@ -2,7 +2,7 @@ export const apiEnvironment = {
     production: false,
     apiUrl: 'http://cloudtecnologia.dynns.com:8001',
     tokenUrl: 'http://cloudtecnologia.dynns.com:8180/realms/CLOUD_TECNOLOGIA/protocol/openid-connect/token',
-    versao: 'v_07.03.2026',
+    versao: 'v_18.03.2026',
     cli_id: 'fluxo-caixa-client',
     cli_secret: 'q80HxrDVjoX93hdA81g3JI4L61FUO2T9'
 };
